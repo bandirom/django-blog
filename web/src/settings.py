ae875c5f678df13741458af0e41aae20b4c10707
Nazarii Romanchenko <nazarii.romanchenko@stemsc.com>
2021-04-17 12:04:50 +0300

    fix bug when login to admin and login in site

--- web/src/settings.py
+++ web/src/settings.py
@@ -116,7 +116,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'dj_rest_auth.jwt_auth.JWTCookieAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
