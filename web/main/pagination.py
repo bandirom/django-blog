6a1161dca60c8f8471d8151075d1b8f049672ece
Nazarii Romanchenko <nazarii.romanchenko@stemsc.com>
2021-05-05 00:34:35 +0300

    update pagination

--- web/main/pagination.py
+++ web/main/pagination.py
@@ -2,7 +2,12 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class DefaultPagination(PageNumberPagination):
-    page_size = 10
+    page_size = 5
     page_query_param = 'page'
     max_page_size = 100
     page_size_query_param = 'page_size'
+
+    def get_paginated_response(self, data):
+        response = super().get_paginated_response(data)
+        response.data.update(self.get_html_context())
+        return response
