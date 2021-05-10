590bf054e1f8d58250859cd82d3ea5cd878010bd
Nazarii Romanchenko <nazarii.romanchenko@stemsc.com>
2021-05-10 12:21:43 +0300

    emailaddress prefetch func

--- web/main/services.py
+++ web/main/services.py
@@ -1,4 +1,7 @@
+from allauth.account.models import EmailAddress
 from django.contrib.auth import get_user_model
+from django.db.models import Prefetch
+
 from main.decorators import except_shell
 
 User = get_user_model()
@@ -10,3 +13,9 @@ class UserService:
     @except_shell((User.DoesNotExist,))
     def get_user(email):
         return User.objects.get(email=email)
+
+    @staticmethod
+    def email_address_prefetch():
+        return Prefetch(
+            'emailaddress_set', queryset=EmailAddress.objects.filter(primary=True), to_attr='email_address'
+        )
