33bce826f7a88726077c9d51cf5c3e9cf02fa80d
Nazarii Romanchenko <nazarii.romanchenko@stemsc.com>
2021-05-10 14:01:22 +0300

    article queryset

--- web/blog/services.py
+++ web/blog/services.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.db.models import Count, QuerySet
+from django.db.models import Count, QuerySet, Prefetch
 
 from .choices import ArticleStatus
 from .models import Category, Article, Comment
@@ -13,12 +13,18 @@ class BlogService:
 
     @staticmethod
     def get_active_articles() -> QuerySet[Article]:
-        return Article.objects.filter(status=ArticleStatus.ACTIVE).annotate(comments_count=Count('comment_set'))
+        comment_prefetch = Prefetch('comment_set', queryset=BlogService.get_comments_queryset(), to_attr='comments')
+        return (Article.objects
+                .select_related('category', 'author')
+                .prefetch_related(comment_prefetch)
+                .filter(status=ArticleStatus.ACTIVE)
+                .annotate(comments_count=Count('comment_set'))
+                )
 
     @staticmethod
     def get_comments_queryset() -> QuerySet[Comment]:
-        return Comment.objects.select_related('article').filter(article__status=ArticleStatus.ACTIVE)
+        return Comment.objects.select_related('user', 'article', 'parent').filter(article__status=ArticleStatus.ACTIVE)
 
     @staticmethod
-    def is_valid_comment_parent(parent_id: int, article: Article):
+    def is_valid_comment_parent(parent_id: int, article: Article) -> bool:
         return Comment.objects.filter(id=parent_id, article=article, parent__isnull=True).exists()
