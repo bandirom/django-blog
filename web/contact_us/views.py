c51293691a963c2dd2e98697b373cb5000d30189
Nazarii Romanchenko <nazarii.romanchenko@stemsc.com>
2021-05-29 11:26:01 +0300

    feedback upd

--- web/contact_us/views.py
+++ web/contact_us/views.py
@@ -1,6 +1,7 @@
 import logging
 
 from rest_framework.generics import CreateAPIView
+from rest_framework.parsers import MultiPartParser
 from rest_framework.permissions import AllowAny
 from .serializers import FeedbackSerializer
 
@@ -10,3 +11,5 @@ logger = logging.getLogger(__name__)
 class FeedbackView(CreateAPIView):
     permission_classes = (AllowAny,)
     serializer_class = FeedbackSerializer
+    parser_classes = (MultiPartParser,)
+    template_name = 'contact_us/index.html'
