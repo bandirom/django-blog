ab4e867ee18bb17d3afc3b46f13572e46c0a5a1d
Nazarii Romanchenko <nazarii.romanchenko@stemsc.com>
2021-06-08 17:59:25 +0300

    like commit

--- web/blog/static/blog/js/likes.js
+++ web/blog/static/blog/js/likes.js
@@ -4,6 +4,7 @@ $(function () {
 });
 
 function like(e) {
+
   e.preventDefault()
   let like = $(this);
   let type = like.data('type');
@@ -12,10 +13,17 @@ function like(e) {
   let dislike = like.next();
   console.log(type, pk, action, dislike)
   // $.ajax({
-    // url:
+  // url:
   // })
 }
 
 function dislike() {
   console.log('dislike')
+  let dislike = $(this);
+  let type = dislike.data('type');
+  let pk = dislike.data('id');
+  let action = dislike.data('action');
+  let like = dislike.prev();
+  console.log(type, pk, action, dislike)
+
 }
