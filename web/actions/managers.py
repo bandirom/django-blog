632853b64c1e910a9b2503e685211b78e33ed1dd
Nazarii Romanchenko <nazarii.romanchenko@stemsc.com>
2021-06-27 23:15:35 +0300

    refactoring

--- web/actions/managers.py
+++ web/actions/managers.py
@@ -1,22 +1,23 @@
 from django.db import models
 from django.db.models import Sum
+from .choices import LikeStatus, LikeObjChoice
 
 
 class LikeDislikeManager(models.Manager):
     use_for_related_fields = True
 
     def likes(self):
-        return self.get_queryset().filter(vote__gt=0)
+        return self.get_queryset().filter(vote=LikeStatus.LIKE)
 
     def dislikes(self):
-        return self.get_queryset().filter(vote__lt=0)
+        return self.get_queryset().filter(vote=LikeStatus.DISLIKE)
 
     def sum_rating(self):
 
         return self.get_queryset().aggregate(Sum('vote')).get('vote__sum') or 0
 
     def articles(self):
-        return self.get_queryset().filter(content_type__model='article').order_by('-articles__pub_date')
+        return self.get_queryset().filter(content_type__model=LikeObjChoice.ARTICLE).order_by('-articles__updated')
 
     def comments(self):
-        return self.get_queryset().filter(content_type__model='comment').order_by('-comments__pub_date')
+        return self.get_queryset().filter(content_type__model=LikeObjChoice.COMMENT).order_by('-comments__updated')
