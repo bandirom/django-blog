039687f9d774044dbbe6e1259d74b99be8211ce2
Nazarii Romanchenko <nazarii.romanchenko@stemsc.com>
2021-07-02 22:37:29 +0300

    refactoring. added file attach

--- web/auth_app/tasks.py
+++ web/auth_app/tasks.py
@@ -1,6 +1,5 @@
-from typing import Union, List
-
-from django.conf import settings
+from typing import Union
+from os import environ
 from django.core.mail import EmailMultiAlternatives
 from django.template import loader
 from django.utils.translation import activate
@@ -11,14 +10,17 @@ from src.celery import app
 
 @app.task()
 def send_information_email(
-    subject: str, html_email_template_name: str,
-    context: dict, to_email: Union[List[str], str], letter_language: str = 'en'
+    subject: str, html_email_template_name: str, context: dict, to_email: Union[list[str], str],
+    letter_language: str = 'en', file_url: str = None, **kwargs
 ):
     activate(letter_language)
     to_email = [to_email] if isinstance(to_email, str) else to_email
-    email_message = EmailMultiAlternatives(subject=subject, to=to_email)
+    email_message = EmailMultiAlternatives(subject=subject, to=to_email, bcc=kwargs.get('bcc'), cc=kwargs.get('cc'))
     html_email = loader.render_to_string(html_email_template_name, context)
     email_message.attach_alternative(html_email, 'text/html')
+    if file_url:
+        file_url = environ.get('APP_HOME', environ.get('HOME')) + file_url
+        email_message.attach_file(file_url, kwargs.get('mimetype'))
     send_email(email_message)
 
 
