8892f969b2cf2c5ab26c042e641eef9df691b42c
Nazarii Romanchenko <nazarii.romanchenko@stemsc.com>
2021-08-07 10:07:11 +0300

    update service methods

--- web/actions/services.py
+++ web/actions/services.py
@@ -4,16 +4,20 @@ from django.conf import settings
 from django.contrib.contenttypes.models import ContentType
 
 from main.decorators import except_shell
-from .models import LikeDislike, Follower
+from .models import LikeDislike, Follower, Action
 from blog.models import Article, Comment
 
 
 class ActionsService:
 
     @staticmethod
+    def get_content_object(model_object):
+        return ContentType.objects.get_for_model(model_object)
+
+    @staticmethod
     @except_shell((LikeDislike.DoesNotExist,))
     def get_like_dislike_obj(object_id: int, user, model_object: Union[Article, Comment]) -> LikeDislike:
-        content_type = ContentType.objects.get_for_model(model_object)
+        content_type = ActionsService.get_content_object(model_object)
         return LikeDislike.objects.get(content_type=content_type, object_id=object_id, user=user)
 
     @staticmethod
@@ -27,3 +31,7 @@ class ActionsService:
     @staticmethod
     def unfollow_user(user, to_user_id: int):
         return user.following.filter(to_user_id=to_user_id).delete()
+
+    @staticmethod
+    def create_action(user, action: str, target):
+        return Action.objects.create(user=user, action=action, content_object=target)
