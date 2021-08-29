22ffbf5b91071a567fa08391f7071552276b0f28
Nazarii Romanchenko <nazarii.romanchenko@stemsc.com>
2021-08-29 16:27:06 +0300

    delete comma

--- web/main/utils.py
+++ web/main/utils.py
@@ -25,4 +25,4 @@ def find_by_key(data: dict, target):
 
 def find_dict_in_list(target: list[dict], dict_key, lookup_value):
     """Find a dict in a list of dict by dict key"""
-    return next(iter(x for x in target if x.get(dict_key) == lookup_value), {},)
+    return next(iter(x for x in target if x.get(dict_key) == lookup_value), {})
