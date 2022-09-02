06f0a822f507b34c2d51f9e0364de56de35fdf01
bandirom <bandirom@ukr.net>
2022-09-02 18:28:10 +0300

    update

--- web/api/v1/urls.py
+++ web/api/v1/urls.py
@@ -4,4 +4,6 @@ app_name = 'v1'
 
 urlpatterns = [
     path('auth/', include('api.v1.auth_app.urls')),
+    path('article/', include('api.v1.blog.urls')),
+    path('contact/', include('api.v1.contact_us.urls')),
 ]
