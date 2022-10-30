5d573647ac54b1d71fd2977c896ab03d593d8991
bandirom <bandirom@ukr.net>
2022-10-30 10:25:57 +0200

    fixing jwt settings

--- web/src/settings.py
+++ web/src/settings.py
@@ -7,6 +7,7 @@ from .additional_settings.logging_settings import *
 from .additional_settings.smtp_settings import *
 from .additional_settings.summernote_settings import *
 from .additional_settings.swagger_settings import *
+from .additional_settings.jwt_settings import *
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
