2c35064198f41b55491d4c16ca4a4f3a1a4d6ea4
Nazarii <romanchenko@procivis.ch>
2023-11-01 14:29:14 +0200

    cleaning

--- web/blog/urls.py
+++ web/blog/urls.py
@@ -1,16 +1,10 @@
 from django.urls import path
-from rest_framework.routers import DefaultRouter
 
-from . import views
 from main.views import TemplateAPIView
 
 app_name = 'blog'
 
-router = DefaultRouter()
-router.register('posts', views.ArticleViewSet, basename='post')
 
 urlpatterns = [
     path('blog/', TemplateAPIView.as_view(template_name='blog/post_list.html'), name='blog-list'),
 ]
-
-urlpatterns += router.urls
