b59283cb8480a6ff3d5dbd4827071462c4af5923
Nazarii <romanchenko@procivis.ch>
2023-11-20 23:10:31 +0200

    cleaning

--- web/conftest.py
+++ web/conftest.py
@@ -1,3 +1,25 @@
+from typing import NamedTuple
+
 import pytest
+from django.contrib.auth import get_user_model
+
+from main.models import UserType
 
 pyteststmark = [pytest.mark.django_db]
+User: UserType = get_user_model()
+
+
+class UserToken(NamedTuple):
+    access_token: str
+    refresh_token: str
+
+
+@pytest.fixture()
+def user() -> User:
+    user = User.objects.create_user(
+        email='harley.quinn@email.com',
+        password='some_p@ssword',
+        first_name='Margot',
+        last_name='Robbie',
+    )
+    return user
